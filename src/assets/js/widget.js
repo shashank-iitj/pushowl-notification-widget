9ab3e816f60322523ea596a058e82a36a22c56a6
radbrawler <radbrawler@gmail.com>
2017-02-18 14:13:41 +0530

    First Task Done

--- src/assets/js/widget.js
+++ src/assets/js/widget.js
@@ -1,30 +1,28 @@
 'use strict';
 
 var pushowl = {
-    init: function () {
-        // TODO: Add your html code here
-        var widget = `
-            <div id="pushowl-widget-container">
-                <div class="pushowl-notification-widget">
-                    <h4>This is a widget</h4>
-                </div>
-            </div>`;
-        // Insert the pushowl widget container
-        document.body.insertAdjacentHTML('afterend', widget);
+  init: function () {
+    // TODO: Add your html code here
+    var widget = $.get("/widget.html").then((result) => {
+      console.log(result)
+      // Insert the pushowl widget container
+      document.body.insertAdjacentHTML('afterend', result);
 
-        this.endpoint = "http://127.0.0.1:3000";
-        var docHead = document.getElementsByTagName("head")[0];
-        this.addCSS(docHead, this.endpoint + "/assets/css/widget.css", false);
-    },
-    addCSS: function (parent, url, sync) {
-        var link = document.createElement("link");
-        link.href = url;
-        link.type = "text/css";
-        link.rel = "stylesheet";
-        link.async = sync;
-        link.media = "screen,print";
-        parent.appendChild(link);
-    }
+      this.endpoint = "http://127.0.0.1:3000";
+      var docHead = document.getElementsByTagName("head")[0];
+      this.addCSS(docHead, this.endpoint + "/assets/css/widget.css", false);
+    })
+
+  },
+  addCSS: function (parent, url, sync) {
+    var link = document.createElement("link");
+    link.href = url;
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.async = sync;
+    link.media = "screen,print";
+    parent.appendChild(link);
+  }
 };
 
 pushowl.init();
