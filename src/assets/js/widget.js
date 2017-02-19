2508a3538cc63e45440133dc6613169ef56f5da2
Abhimanyu Singh Gaur <gaur.1@iitj.ac.in>
2017-02-19 15:55:49 +0530

    Final Tweaks

--- src/assets/js/widget.js
+++ src/assets/js/widget.js
@@ -3,11 +3,13 @@
 var pushowl = {
     init: function () {
         // TODO: Add your html code here
+        this.endpoint = "http://127.0.0.1:3000";
+        
         var widget = `
             <div id="pushowl-widget-container">
                 <div class="pushowl-notification-widget">
                     <div class="pushowl-widget-bell-container">
-                        <img src="/assets/images/ic_notifications_white_24px.svg" width="20" height="20" />
+                        <img src="` + this.endpoint + `/assets/images/ic_notifications_white_24px.svg" width="20" height="20" />
                     </div>
                     <div class="pushowl-widget-content">
                         <div class="pushowl-widget-message">Get notification on your computer for important events and updates.</div>
@@ -21,10 +23,9 @@ var pushowl = {
         // Insert the pushowl widget container
         document.body.insertAdjacentHTML('afterend', widget);
 
-        this.endpoint = "http://127.0.0.1:3000";
         var docHead = document.getElementsByTagName("head")[0];
         this.addCSS(docHead, this.endpoint + "/assets/css/widget.css", false);
-        this.addCSS(docHead, "https://fonts.googleapis.com/css?family=Roboto", false);
+        this.addCSS(docHead, "https://fonts.googleapis.com/css?family=Montserrat", false);
     },
     addCSS: function (parent, url, sync) {
         var link = document.createElement("link");
